ccdd0d77b3bafa259c3162898bd285e232e2b1fb
fanyanbo <fanyanbo@skyworth.com>
2018-12-05 11:40:55 +0800

    初始化工程

---
+++ pages/index/index.js
@@ -0,0 +1,25 @@
+Page({
+  data: {
+  
+  },
+
+  onLoad(options) {
+    console.log('first onLoad监听页面加载');
+  },
+
+  onReady() {
+    console.log('first onReady监听页面初次渲染完成');
+  },
+
+  onShow() {
+    console.log('first onShow监听页面显示');
+  },
+
+  onHide() {
+    console.log('first onHide监听页面隐藏');
+  },
+
+  onUnload() {
+    console.log('first onUnload监听页面卸载');
+  }
+});
