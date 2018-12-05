1687379d73888a22f35dff316fae94bc16da7ffd
fanyanbo <fanyanbo@skyworth.com>
2018-12-05 14:55:07 +0800

    修改主页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,33 @@
 Page({
   data: {
-  
+    navbarActiveIndex: 0,
+    navbarTitle: [
+      "主页",
+      "发现",
+      "我的"
+    ]
+  },
+
+  /**
+ * 点击导航栏
+ */
+  onNavBarTap: function (event) {
+    // 获取点击的navbar的index
+    let navbarTapIndex = event.currentTarget.dataset.navbarIndex
+    // 设置data属性中的navbarActiveIndex为当前点击的navbar
+    this.setData({
+      navbarActiveIndex: navbarTapIndex
+    })
+  },
+
+  /**
+   * 
+   */
+  onBindAnimationFinish: function ({ detail }) {
+    // 设置data属性中的navbarActiveIndex为当前点击的navbar
+    this.setData({
+      navbarActiveIndex: detail.current
+    })
   },
 
   onLoad(options) {
