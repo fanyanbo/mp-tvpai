ec2faef40ec624d9500ad3771e6691f93e15aa81
yb <yuanbo01@skyworth.com>
2019-03-16 15:02:29 +0800

    设备绑定-编辑-删除确认弹窗：改用微信小程序平台API: wx.showActionSheet()

--- pages/delete/delete.js
+++ pages/delete/delete.js
@@ -10,9 +10,7 @@ Page({
     contents: '',
     deviceId: '',
     bind: '',
-    deviceName:'',
-    chooseSize: false,
-    animationData: {}
+    deviceName:''
   },
   deleteName: function (e) {
     let that = this
@@ -58,49 +56,20 @@ Page({
     }, "")
   },
   chooseSezi: function (e) {
-    var that = this;
-    // 创建一个动画实例
-    var animation = wx.createAnimation({
-      duration: 500,
-      timingFunction: 'linear'
+    let that = this
+    wx.showActionSheet({
+      itemList: ['确定删除'],
+      itemColor: '#E64340',
+      success(res) {
+        console.log('success :'+ JSON.stringify(res))
+        if(res.tapIndex == 0){
+          that.deleteName()
+        }
+      },
+      fail(res) {
+        console.log('fail :' + JSON.stringify(res))
+      }
     })
-    that.animation = animation
-    // 先在y轴偏移，然后用step()完成一个动画
-    animation.translateY(200).step()
-    // 用setData改变当前动画
-    that.setData({
-      // 通过export()方法导出数据
-      animationData: animation.export(),
-      // 改变view里面的Wx：if
-      chooseSize: true
-    })
-    // 设置setTimeout来改变y轴偏移量，实现有感觉的滑动
-    setTimeout(function () {
-      animation.translateY(0).step()
-      that.setData({
-        animationData: animation.export()
-      })
-    }, 200)
-  },
-  hideModal: function (e) {
-    var that = this;
-    var animation = wx.createAnimation({
-      duration: 1000,
-      timingFunction: 'linear'
-    })
-    that.animation = animation
-    animation.translateY(200).step()
-    that.setData({
-      animationData: animation.export()
-
-    })
-    setTimeout(function () {
-      animation.translateY(0).step()
-      that.setData({
-        animationData: animation.export(),
-        chooseSize: false
-      })
-    }, 200)
   },
   /**
    * 生命周期函数--监听页面加载
@@ -127,11 +96,6 @@ Page({
    */
   onShow: function () {
     console.log("返回")
-    let that = this;
-    that.setData({
-      chooseSize: false
-    })
-
   },
 
   /**
