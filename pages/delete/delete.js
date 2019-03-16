903f170d855c227ebf2cf7a9e2ad017e81e7516b
yb <yuanbo01@skyworth.com>
2019-03-16 16:15:13 +0800

    回退wx.showActionSheet的修改，因为兼容性问题 Revert "设备绑定-编辑-删除确认弹窗：改用微信小程序平台API: wx.showActionSheet()"
    This reverts commit ec2faef40ec624d9500ad3771e6691f93e15aa81.

--- pages/delete/delete.js
+++ pages/delete/delete.js
@@ -10,7 +10,9 @@ Page({
     contents: '',
     deviceId: '',
     bind: '',
-    deviceName:''
+    deviceName:'',
+    chooseSize: false,
+    animationData: {}
   },
   deleteName: function (e) {
     let that = this
@@ -56,34 +58,49 @@ Page({
     }, "")
   },
   chooseSezi: function (e) {
-    let that = this
-    wx.getSystemInfo({
-      success: function(res) {
-        console.log('getsysteminfo  '+JSON.stringify(res))
-        let itemList
-        if(res.platform == 'ios') {
-          itemList = ['确定删除']
-        }else {
-          itemList = ['确定删除', '取消']
-        }
-        wx.showActionSheet({
-          itemList: itemList,
-          // itemColor: '#E64340',
-          success(res) {
-            console.log('success :' + JSON.stringify(res))
-            if (res.tapIndex == 0) {
-              that.deleteName()
-            }else if(res.tapIndex == 1){
-              //空操作
-              console.log('取消')
-            }
-          },
-          fail(res) {
-            console.log('fail :' + JSON.stringify(res))
-          }
-        })
-      },
+    var that = this;
+    // 创建一个动画实例
+    var animation = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'linear'
     })
+    that.animation = animation
+    // 先在y轴偏移，然后用step()完成一个动画
+    animation.translateY(200).step()
+    // 用setData改变当前动画
+    that.setData({
+      // 通过export()方法导出数据
+      animationData: animation.export(),
+      // 改变view里面的Wx：if
+      chooseSize: true
+    })
+    // 设置setTimeout来改变y轴偏移量，实现有感觉的滑动
+    setTimeout(function () {
+      animation.translateY(0).step()
+      that.setData({
+        animationData: animation.export()
+      })
+    }, 200)
+  },
+  hideModal: function (e) {
+    var that = this;
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'linear'
+    })
+    that.animation = animation
+    animation.translateY(200).step()
+    that.setData({
+      animationData: animation.export()
+
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      that.setData({
+        animationData: animation.export(),
+        chooseSize: false
+      })
+    }, 200)
   },
   /**
    * 生命周期函数--监听页面加载
@@ -110,6 +127,11 @@ Page({
    */
   onShow: function () {
     console.log("返回")
+    let that = this;
+    that.setData({
+      chooseSize: false
+    })
+
   },
 
   /**
