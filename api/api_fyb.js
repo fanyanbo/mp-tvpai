8fa09d8f598309c59360acadf897b0274679584a
fanyanbo <fanyanbo@coocaa.com>
2019-03-19 14:56:24 +0800

    更改我的页面授权的流程

--- api/api_fyb.js
+++ api/api_fyb.js
@@ -10,6 +10,7 @@ const collectUrl = baseUrl_tvpai + "video/client/collect/add"; //收藏或取消
 const getCollectedListUrl = baseUrl_tvpai + "video/client/collect/list"; //获取已收藏影片列表
 const getOneClassifyUrl = baseUrl_tvpai + "video/client/longvideo/oneclassify";// 获取一级标签分类
 const getRecommendListUrl = baseUrl_tvpai + "video/client/longvideo/recommendlist";// 获取首页推荐
+const getHistoryListUrl = baseUrl_tvpai + "video/client/pushhistory/list";//获取历史列表
 
 // 微信业务后台接口
 const getBindDeviceListUrl = baseUrl_wx + "wxUserDeviceAPI/bindDeviceList.coocaa";//获取已绑定设备列表
@@ -32,5 +33,6 @@ module.exports = {
   getBannerDataUrl: getBannerDataUrl,
   getSessionUrl: getSessionUrl,
   getUserInfoUrl: getUserInfoUrl,
-  bindDeviceUrl: bindDeviceUrl
+  bindDeviceUrl: bindDeviceUrl,
+  getHistoryListUrl: getHistoryListUrl
 }
--- pages/my/my.js
+++ pages/my/my.js
@@ -1,7 +1,5 @@
-const utils = require('../../utils/util.js');
 const utils_fyb = require('../../utils/util_fyb');
-const api = require('../../api/api.js');
-const app = getApp();
+const api_fyb = require('../../api/api_fyb');
 
 Page({
   data: {
@@ -11,62 +9,19 @@ Page({
     },
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  getMovie: function () {
-    let that = this
-    const secret = app.globalData.secret
-    const vuid = wx.getStorageSync('wxopenid')
-    console.log(vuid);
-    const params = { "appkey": app.globalData.appkey, "time": app.globalData.time, "tv_source": app.globalData.tvSource, "version_code": app.globalData.version_code, "vuid": vuid }
-    console.log(params);
-    const sign = utils.encryptionIndex(params, secret)
-    const url = api.pushhistorylistUrl
-    let data = {
-      appkey: app.globalData.appkey,
-      vuid: vuid,
-      time: app.globalData.time,
-      tv_source: app.globalData.tvSource,
-      version_code: app.globalData.version_code,
-      sign: sign
-    }
-    utils.postLoading(url, 'GET', data, function (res) {
-      console.log("推送历史")
-      console.log(res)
-      if (res.data.data) {
-        let withinList = res.data.data.movies_within_serven_days
-        let overList = res.data.data.movies_over_serven_days
-        if (withinList.length == 0) {
-          that.setData({
-            historyList: overList
-          })
-        } else {
-          that.setData({
-            historyList: withinList
-          })
-        }
-      }
-    }, function (res) {
-      console.log('streams fail:',res)
-      wx.showToast({
-        title: '加载数据失败',
-      })
-    }, function (res) {
-      console.log('streams complete')
-    }, '')
-  },
+
   onLoad: function () {
-    this.getMovie();
+    this.getMovieHistoryList();
   },
-  onShow: function () {
-  },
-  onPullDownRefresh: function () {
-    this.getMovie()
-    wx.stopPullDownRefresh();
-  },
-  onShareAppMessage: function () {
-  },
+
+  // 设备绑定入口暂未使用，但推送历史需要使用
   bindGetUserInfo(e) {
-    let that = this;
     console.log('canIUse', this.data.canIUse, e)
+    if (!e.detail.userInfo) {
+      // 用户拒绝直接返回授权框
+      return;
+    }
+    let that = this;
     let ccsession = wx.getStorageSync("cksession");
     console.log('bindGetUserInfo ccsession', ccsession);
     if (ccsession == null || ccsession === '') {
@@ -88,7 +43,7 @@ Page({
               }
             }
           }, function (res) {
-            console.log('error', res)
+            console.log('getSessionByCode error', res)
           });
         }
       });
@@ -106,51 +61,44 @@ Page({
       url: '../home/home',
     })
   },
+
   // 跳转播放历史页面
   jumpHistoryPage: function () {
     wx.navigateTo({
       url: '../history/history',
     })
   },
-    // jumpurl: function (e) {
-  //   let ccsession = wx.getStorageSync("cksession")
-  //   console.log("jumpurl ccsession:" + ccsession);
-  //   if (ccsession != null && ccsession != undefined && ccsession !== '') {
-  //     var type = e.currentTarget.dataset.type
-  //     if (type === "home") {
-  //       wx.navigateTo({
-  //         url: '../home/home',
-  //       })
-  //     } else if (type === "history") {
-  //       wx.navigateTo({
-  //         url: '../history/history',
-  //       })
-  //     }
-  //   } else {
-  //     wx.login({
-  //       success: function (res) {
-  //         const code = res.code;
-  //         console.log("wx.login", JSON.stringify(res));
-  //         wx.getUserInfo({
-  //           success: function (res) {
-  //             console.log('wx.getUserInfo', res)
-  //             let encryptedData = res.encryptedData;
-  //             let iv = res.iv;
-  //             let rawData = res.rawData;
-  //             let signature = res.signature;
-  //             app.globalData.userInfo = res.userInfo;
-  //             wx.setStorageSync('userInfo', res.userInfo);
-  //             typeof cb == "function" && cb(app.globalData.userInfo);
-  //             appJs.login(rawData, code, encryptedData, iv, signature);
-  //           },
-  //           fail: function (err) {
-  //             console.log("我的页面，获取用户信息失败", err);
-  //           }
-  //         })
-  //       }
-  //     })
-  //   }
-  // },
+
+  getMovieHistoryList: function () {
+    let that = this;
+    let vuid = wx.getStorageSync('wxopenid');
+    console.log('vuid:',vuid);
+    if (vuid == null || vuid === '') return;
+    let srcParams = {"vuid": vuid };
+    let desParams = utils_fyb.paramsAssemble_tvpai(srcParams);
+    console.log(desParams);
+    utils_fyb.request(api_fyb.getHistoryListUrl, 'GET', desParams, 
+      function (res) {
+        console.log("getMovieHistoryList success", res);
+        if (res.data.data) {
+          let withinList = res.data.data.movies_within_serven_days
+          let overList = res.data.data.movies_over_serven_days
+          if (withinList.length == 0) {
+            that.setData({
+              historyList: overList
+            })
+          } else {
+            that.setData({
+              historyList: withinList
+            })
+          }
+        }
+      },
+      function (res) {
+        console.log("getMovieHistoryList error", res);
+      }
+    )
+  },
 })
 
 
