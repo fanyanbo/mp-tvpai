d4873728365ea9a20c9b3ba5069ab8da2324c62b
yb <yuanbo01@skyworth.com>
2019-03-22 20:23:44 +0800

    删除测试语句

--- components/remotecontrol/index.js
+++ components/remotecontrol/index.js
@@ -597,13 +597,3 @@ Component({
     console.log('remotecontrol component detached()')
   }
 })
-
- const f = () => console.log('now');
- (
-   () => new Promise(
-     resolve => resolve(f())
-   )
- )();
-
- console.log('next')
- 
