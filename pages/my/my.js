85f576f696a19cca9acdbf421cc85da211aaa2ec
fanyanbo <fanyanbo@coocaa.com>
2019-04-04 16:02:53 +0800

    解决剧集updated_segment

--- pages/my/my.js
+++ pages/my/my.js
@@ -89,8 +89,8 @@ Page({
   },
 
   // clearStorage: function () {
-  //   wx.setStorageSync("cksession", "");
-  //   console.log('clearStorage session = ' + wx.getStorageSync("cksession"))
+  //   wx.setStorageSync("new_cksession", "");
+  //   console.log('clearStorage session = ' + wx.getStorageSync("new_cksession"))
   // },
 
   // 获取推送历史列表
@@ -137,10 +137,6 @@ Page({
       console.log("getLikeList error", res);
     })
   },
-
-
-
-
 })
 
 
