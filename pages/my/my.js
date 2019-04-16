3f7f0faf906fc0cd6f9d7e239c6a95173292bdd6
fanyanbo <fanyanbo@coocaa.com>
2019-04-16 09:26:23 +0800

    同步代码

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,66 +1,109 @@
-// pages/my/my.js
+//index.js
+//获取应用实例
+const app = getApp()
+
 Page({
+    data: {
+        motto: 'Hello World',
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        navData:[
+            {
+                text: '首页'
+            },
+            {
+                text: '健康'
+            },
+            {
+                text: '情感'
+            },
+            {
+                text: '职场'
+            },
+            {
+                text: '育儿'
+            },
+            {
+                text: '纠纷'
+            },
+            {
+                text: '青葱'
+            },
+            {
+                text: '上课'
+            },
+            {
+                text: '下课'
+            }
+        ],
+        currentTab: 0,
+        navScrollLeft: 0
+    },
+    //事件处理函数
+    onLoad: function () {
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            })
+        } else if (this.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        } else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
+        }
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+        wx.getSystemInfo({
+            success: (res) => {
+                this.setData({
+                    pixelRatio: res.pixelRatio,
+                    windowHeight: res.windowHeight,
+                    windowWidth: res.windowWidth
+                })
+            },
+        })       
+    },
+    switchNav(event){
+        var cur = event.currentTarget.dataset.current; 
+        console.log('switchNav:' + cur)
+        //每个tab选项宽度占1/5
+        var singleNavWidth = this.data.windowWidth / 5;
+        //tab选项居中                            
+        this.setData({
+            navScrollLeft: (cur - 2) * singleNavWidth
+        })      
+        if (this.data.currentTab == cur) {
+            return false;
+        } else {
+            this.setData({
+                currentTab: cur
+            })
+        }
+    },
+    switchTab(event){
+        var cur = event.detail.current;
+        console.log('switchTab:' + cur)
+        var singleNavWidth = this.data.windowWidth / 5;
+        this.setData({
+            currentTab: cur,
+            navScrollLeft: (cur - 2) * singleNavWidth
+        });
+    }
 })
