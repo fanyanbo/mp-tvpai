62a3806ea387b46f79b4240066f7afbd985fc117
fanyanbo <fanyanbo@coocaa.com>
2019-09-20 10:26:48 +0800

    1.规范化我的页面 2.适配自定义导航栏和tabbar

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,59 +1,59 @@
-const utils_fyb = require('../../utils/util_fyb');
-const api_fyb = require('../../api/api_fyb');
-let appJs = require('../../app');
-let app = getApp()
+// author: fanyanbo
+// email: fanyanbo@coocaa.com
+// date: 2019-09-20
+// todo: 1.命名优化，2.代码优化 3.适配自定义导航栏
+
+const utils_fyb = require('../../utils/util_fyb')
+const api_fyb = require('../../api/api_fyb')
+const app = getApp()
+
 Page({
   data: {
     userInfo: {
       nickName: '你好',
       avatarUrl: '../../images/man.png',
-      isShow:false
+      isDevConnected: false
     },
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    isShowTips:true,
-    bIphoneFullScreenModel:false
+    isShowTips: true,
+    bIphoneFullScreenModel: false
   },
 
   onLoad: function () {
     console.log('onLoad');
     if (app.globalData.deviceId != null) {
       this.setData({
-        isShow: true
+        isDevConnected: true
       })
     }
-
-    // this.getMovieHistoryList();
   },
 
   onShow: function () {
     console.log('onShow');
-    this.setData({
-      isShowTips: app.globalData.isShowTips,
-      bIphoneFullScreenModel: app.globalData.bIphoneFullScreenModel
-    });
-    this.getMovieHistoryList();
-    this.getLikeList();
-    if (app.globalData.deviceId != null) {
-      this.setData({
-        isShow: true
-      })
-    }else {
-      this.setData({
-        isShow: false
+
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+      // 切换到“我的”tab，设置选中状态
+      this.getTabBar().setData({
+        selected: 1 // 这个数是tabBar从左到右的下标，从0开始
       })
     }
 
-    //TabBar选中状态
-    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
-      this.getTabBar().setData({
-        selected: 1
-      })
-      console.log("页面切换到我的")
-    }
+    let _isDevConnected = app.globalData.deviceId != null ? true : false
+    this.setData({
+      isShowTips: app.globalData.isShowTips,
+      bIphoneFullScreenModel: app.globalData.bIphoneFullScreenModel,
+      isDevConnected: _isDevConnected
+    });
+
+    // 获取历史和收藏列表
+    this.getHistoryList();
+    this.getFavoriteList();
   },
-  clearStorage:function(){
+
+  clearStorage: function () {
     wx.setStorageSync('new_cksession', "");
   },
+
   // 设备绑定和推送历史入口暂未使用
   bindGetUserInfo(e) {
     console.log('canIUse', this.data.canIUse, e)
@@ -92,72 +92,55 @@ Page({
   },
   // 跳转设备绑定页面
   handleJumpPage: function (e) {
-    console.log(e.currentTarget.dataset.type);
-    if (e.currentTarget.dataset.type === 'home') {
-      wx.navigateTo({
-        url: '../home/home',
-      })
-    } else if (e.currentTarget.dataset.type === 'history') {
-      let ccsession = wx.getStorageSync("new_cksession")
-      if (ccsession == null || ccsession === '') {
-        wx.navigateTo({
-          url: '../home/home',
-        })
-      } else {
-        wx.navigateTo({
-          url: '../history/history',
-        })
-      }
-    }
+    console.log(e.currentTarget.dataset.type)
+    let _type = e.currentTarget.dataset.type
+    let _session = wx.getStorageSync("new_cksession")
+    let _path = (_type === 'history' && _session != null) ? '../history/history' : '../home/home'
+    console.log(_type, _session, _path);
+    wx.navigateTo({
+      url: _path,
+    })
   },
 
-  // clearStorage: function () {
-  //   wx.setStorageSync("new_cksession", "");
-  //   console.log('clearStorage session = ' + wx.getStorageSync("new_cksession"))
-  // },
-
-  // 获取推送历史列表
-  getMovieHistoryList: function () {
+  // 获取历史列表
+  getHistoryList: function () {
     let vuid = wx.getStorageSync('wxopenid');
-    console.log('vuid:',vuid);
+    console.log('获取openid：', vuid);
     if (vuid == null || vuid === '') return;
-    let srcParams = {"vuid": vuid };
+    let srcParams = { "vuid": vuid };
     let desParams = utils_fyb.paramsAssemble_tvpai(srcParams);
-  //  console.log(desParams);
     utils_fyb.requestP(api_fyb.getHistoryListUrl, desParams).then(res => {
-      console.log("getMovieHistoryList success", res);
+      console.log("获取历史列表成功：", res);
       if (res.data.data) {
         let withinList = res.data.data.movies_within_serven_days
         let overList = res.data.data.movies_over_serven_days
         let historyList = withinList.concat(overList);
-          this.setData({
-            historyList: historyList
-          })
-
+        this.setData({
+          historyList: historyList
+        })
       }
     }).catch(res => {
-      console.log("getMovieHistoryList error", res);
+      console.log("获取历史列表发生错误：", res);
     })
   },
 
-  // 获取推送历史列表
-  getLikeList: function () {
+  // 获取收藏列表
+  getFavoriteList: function () {
     let vuid = wx.getStorageSync('wxopenid');
-    console.log('vuid:',vuid);
+    console.log('获取openid：', vuid);
     if (vuid == null || vuid === '') return;
-    let srcParams = { "vuid": vuid,"video_type":1};
+    let srcParams = { "vuid": vuid, "video_type": 1 };
     let desParams = utils_fyb.paramsAssemble_tvpai(srcParams);
     console.log(desParams);
     utils_fyb.requestP(api_fyb.getCollectedListUrl, desParams).then(res => {
-      console.log("我的喜欢 success", res);
+      console.log("获取收藏列表成功：", res);
       if (res.data.data) {
-          this.setData({
-            likeList: res.data.data
-          })
-
+        this.setData({
+          likeList: res.data.data
+        })
       }
     }).catch(res => {
-      console.log("getLikeList error", res);
+      console.log("获取收藏列表发生错误：", res);
     })
   },
 })
