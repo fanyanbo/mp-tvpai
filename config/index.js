0e3db0c5285cce7bdf403efe3cb37519ee0a9192
fanyanbo <fanyanbo@coocaa.com>
2019-11-01 10:52:09 +0800

    切换正式环境

--- config/index.js
+++ config/index.js
@@ -2,15 +2,15 @@
  * author : fanyanbo@coocaa.com
  */
 
-// const env = 'prod';
-const env = 'dev';
+const env = 'prod';
+// const env = 'dev';
 
 const config = (env === 'prod') ? {
     env : env,
     baseUrl_nj: "https://user.coocaa.com/",
     baseUrl_tvpai: "https://tvpi.coocaa.com/",
     baseUrl_wx: "https://wx.coocaa.com/",               //微信url(hengyanyan)
-    baseUrl_sz: "https://dev-api-business.skysrt.com/", //产品包url (chenzonghui)
+    baseUrl_sz: "https://api-business.skysrt.com/",     //产品包url (chenzonghui)
     baseUrl_pay: "https://pay.coocaa.com/",             //支付url(chenyuan)
     baseUrl_acct: "https://passport.coocaa.com/",       //账户url(chenxiguang)
     baseUrl_allowance: "https://jintie.coocaa.com/",     //津贴url(zhangyuhao)
